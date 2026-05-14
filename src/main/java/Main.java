83f7c3fe29350fe53346f9831a5de7a324fdf301
Ana Clarissi <anaclarissicruz@gmail.com>
2026-05-14 11:02:44 -0300

    feat: reading arguments from terminal on Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+
+            System.out.println("Usage: github-activity <username>");
+
+            return;
+
+        }
+
+        System.out.printf("Searching for user activities: %s", args[0]);
+
+    }
+
 }
